e37edf1ac9e68785fb1d84db24d7a090a1366525
Maaltas <107067687+Maaltas@users.noreply.github.com>
2023-02-08 11:34:51 +0100

    Update Ubicacio.java

--- Ubicacio.java
+++ Ubicacio.java
@@ -2,31 +2,78 @@ package Sessio1Practica2;
 
 import java.util.Objects;
 
-public class Ubicacio {
+class Ubicacio {
     private int numCarrer;
     private String nomCarrer;
     private SenyalTransit [] senyals;
     private int numSenyals;
     private int maxSenyals;
 
-    public Ubicacio(int Maxim, String carrer, int numero){
+    Ubicacio(int Maxim, String carrer, int numero){
         maxSenyals=Maxim;
         nomCarrer=carrer;
         numCarrer=numero;
         senyals = new SenyalTransit[maxSenyals];
         numSenyals=0;
     }
-    public String getNomCarrer(){
+    String getNomCarrer(){
         return nomCarrer;
     }
-    public int getNumCarrer(){
+    int getNumCarrer(){
         return numCarrer;
     }
-    public int getNumSenyals(){
+    int getNumSenyals(){
         return numSenyals;
     }
-    public boolean iguals (Ubicacio u){
+    boolean iguals (Ubicacio u){
         return u.getNomCarrer().equals(this.getNomCarrer()) && u.numCarrer == this.numCarrer;
     }
-
+    SenyalTransit getSenyal(int quin){
+        if (quin>numSenyals){
+            return null;
+        } else {
+            return this.senyals[quin];
+        }
+    }
+    boolean afegirSenyal (SenyalTransit c){
+        if (maxSenyals==numSenyals){
+            return false;
+        } else {
+            for (int x=0; x<maxSenyals; x++){
+                if (senyals[x].equals(c)){
+                    return false;
+                } else {
+                    if (senyals[x]==null){
+                        senyals[x]=c;
+                        numSenyals++;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    boolean treureSenyal (SenyalTransit c) {
+        for (int j = 0; j < numSenyals; j++) {
+            if (senyals[j].equals(c)){
+                senyals[j]=senyals[numSenyals-1];
+                numSenyals--;
+                senyals[numSenyals]=null;
+                return true;
+            }
+        }
+        return false;
+    }
+    String getSenyals(){
+        String s = "";
+        for (int x=0; x<numSenyals; x++){
+            if(x==0 || x==numSenyals-1){
+                s=this.getNomCarrer();
+            } else {
+                s=s.concat(senyals[x].getCodi()) + " - ";
+            }
+                  
+        }
+        return s;
+    }
 }
+
