d9a31c4873c4abe2fc002bd6399ec9d419d5e7b7
Maaltas <107067687+Maaltas@users.noreply.github.com>
2023-02-14 21:54:23 +0100

    Create Poblacio.java

---
+++ Poblacio.java
@@ -0,0 +1,10 @@
+package Sessio2;
+
+public class Poblacio {
+    // f0 = emagatzema de forma consecutiva les ubicacions amb noms de carrers amb intervals [‘a’,’h’];
+    // f1 = emagatzema de forma consecutiva les ubicacions amb noms de carrers amb intervals [‘i’,’n’];
+    // f2 = emagatzema de forma consecutiva les ubicacions amb noms de carrers amb intervals [‘o’,’z’];
+    private String nom;
+    private int numH;
+
+}
